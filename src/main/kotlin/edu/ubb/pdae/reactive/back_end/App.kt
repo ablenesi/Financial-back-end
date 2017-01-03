d7c720d301bdd0cf50c597d0a2d3a4e744269901
Attila Blenesi <battila@halcyonmobile.com>
2017-01-03 19:35:51 +0200

    Implement initial spring boot setup

--- src/main/kotlin/edu/ubb/pdae/reactive/back_end/App.kt
+++ src/main/kotlin/edu/ubb/pdae/reactive/back_end/App.kt
@@ -1,9 +1,16 @@
 package edu.ubb.pdae.reactive.back_end
 
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
 /**
  * @autor Attila Blenesi ablenesi@gmail.com
  */
+@SpringBootApplication
+open class App{
+
+}
 
 fun main(args: Array<String>) {
-    println("Hello Kotlin!")
+    SpringApplication.run(App::class.java, *args)
 }
